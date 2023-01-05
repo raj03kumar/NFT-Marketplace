fbe529f3bc986282d09ec00b30e37aa2805a6c2b
Raj <singhraj1553@outlook.com>
2023-01-05 20:32:35 +0530

    Disabled inspect element

--- main.js
+++ main.js
@@ -6,3 +6,25 @@ const menuBtn = document.querySelector('.menu-btn');
 menuBtn.addEventListener('click', ()=>{
     menu.classList.toggle('menu-open');
 });
+
+//disable the inspect from webpage
+document.addEventListener("contextmenu", function(e){
+    e.preventDefault(); //this prevents right click
+});
+document.onkeydown=function(e){
+    if(event.keycode==123){
+        return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode=="I".charCodeAt(0)){
+        return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode=="C".charCodeAt(0)){
+        return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode=="J".charCodeAt(0)){
+        return false;
+    }
+    if(e.ctrlKey && e.keyCode=="U".charCodeAt(0)){
+        return false;
+    }
+};
